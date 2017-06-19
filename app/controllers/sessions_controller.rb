4c07822a07130d1e27fa4cd194993fa15b3ccfc3
Neeraj Chandra <nchandra324@gmail.com>
2017-06-19 20:46:30 +0700

    add user auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user
+      if @user.authenticate(params[:password])
+        flash[:success] = "Welcome! Signed in."
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        flash.now[:error] = "Wrong password!"
+        render 'new'
+      end
+    else
+      flash.now[:error] = "User not found."
+      redirect_to action: 'new', show_hint: true
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, flash: {success: "Logged Out"}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index' => 'home#index'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  delete 'log_out' => 'sessions#destroy'
+
+
+
+  root 'home#index'
 end
